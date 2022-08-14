40580f9463128fea26a4b52217ff95c02b820cbd
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2022-08-14 18:51:09 +0400

    0.22.0 update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use similari::examples::BoxGen2;
-use similari::prelude::IoUSort;
+use similari::prelude::PositionalMetricType::IoU;
+use similari::prelude::Sort;
 use similari::trackers::sort::DEFAULT_SORT_IOU_THRESHOLD;
 use similari::utils::bbox::BoundingBox;
 
 fn main() {
-    let mut tracker = IoUSort::new(1, 10, 1, DEFAULT_SORT_IOU_THRESHOLD);
+    let mut tracker = Sort::new(1, 10, 1, IoU(DEFAULT_SORT_IOU_THRESHOLD), None);
 
     let pos_drift = 1.0;
     let box_drift = 0.2;
@@ -12,10 +13,10 @@ fn main() {
     let mut b2 = BoxGen2::new_monotonous(10.0, 10.0, 12.0, 18.0, pos_drift, box_drift);
 
     for _ in 0..10 {
-        let obj1b = b1.next().unwrap();
-        let obj2b = b2.next().unwrap();
-        let _tracks = tracker.predict(&[obj1b.into(), obj2b.into()]);
-        //eprintln!("Tracks: {:#?}", _tracks);
+        let obj1b = (b1.next().unwrap().into(), Some(1));
+        let obj2b = (b2.next().unwrap().into(), Some(2));
+        let _tracks = tracker.predict(&[obj1b, obj2b]);
+        eprintln!("Tracks: {:#?}", _tracks);
     }
 
     tracker.skip_epochs(2);
